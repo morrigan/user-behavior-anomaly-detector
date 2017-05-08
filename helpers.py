2112ebdfde92a17fa1c37403858e15615f92165c
morrigan <belma.dzi@gmail.com>
2017-05-08 21:49:30 +0200

    OCSVM loading

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
 import np
 import random
 import tensorflow as tf
-import numpy, os
+import numpy, os, time
+
 
 def read_data(filename):
     actions = []
@@ -17,7 +18,7 @@ def read_data(filename):
         actions.append(action)
         labels.append(label)
         lengths.append(action_len)
-        #print action, action_len, label
+        # print action, action_len, label
     return actions, lengths, labels
 
 
@@ -28,25 +29,28 @@ def read_single_example(filename_queue):
         serialized_example,
         features={
             'action': tf.FixedLenFeature([200], tf.int64),
-            #'action_len': tf.FixedLenFeature([], tf.int64),
+            # 'action_len': tf.FixedLenFeature([], tf.int64),
             'label': tf.FixedLenFeature([1], tf.int64),
         })
     print features['action']
     return features['action'], features['label']
 
+
 def get_random_minibatch_indices(n_examples, batch_size):
     indices = range(n_examples)
     random.shuffle(indices)
-    num_batches = n_examples/batch_size
+    num_batches = n_examples / batch_size
     minibatch_indices = np.zeros(shape=(num_batches, batch_size), dtype='int64')
     for b_i in range(num_batches):
         for ex_i in range(batch_size):
-            minibatch_indices[b_i] = indices[b_i*batch_size:(b_i+1)*batch_size]
+            minibatch_indices[b_i] = indices[b_i * batch_size:(b_i + 1) * batch_size]
     return minibatch_indices
+
 
 ## Operations
 def lrelu(x, leak=0.2, name="lrelu"):
-  return tf.maximum(x, leak*x)
+    return tf.maximum(x, leak * x)
+
 
 def linear(input_, output_size, layer_scope, stddev=0.02, bias_start=0.0):
     if (input_.count):
@@ -56,47 +60,57 @@ def linear(input_, output_size, layer_scope, stddev=0.02, bias_start=0.0):
     else:
         shape = input_
 
-
     with tf.variable_scope(layer_scope):
         matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
                                  tf.random_normal_initializer(stddev=stddev))
         bias = tf.get_variable("bias", [output_size],
-            initializer=tf.constant_initializer(bias_start))
+                               initializer=tf.constant_initializer(bias_start))
         return tf.matmul(input_, matrix) + bias
 
 
 # Convert an array of values into a dataset matrix
-def convert_dataset(dataset, look_back = 3):
-	dataX, dataY = [], []
-	for i in range(len(dataset) - look_back - 1):
-		a = dataset[i:(i + look_back)]
-		b = dataset[i + look_back]
-		dataX.append(a)
-		dataY.append(b)
-	return numpy.array(dataX), numpy.array(dataY)
+def convert_dataset(dataset, look_back=3):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back)]
+        b = dataset[i + look_back]
+        dataX.append(a)
+        dataY.append(b)
+
+    return numpy.array(dataX), numpy.array(dataY)
+
+def get_real_predictions(dataset):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - 1):
+        b = dataset[i + 1]
+        dataX.append(dataset[i])
+        dataY.append(b)
+
+    return numpy.array(dataX), numpy.array(dataY)
 
 
 def collection_values_to_array(dataset):
-	dataset = numpy.array(dataset)
-	new_dataset = []
-	for row in dataset:
-		row_array = numpy.array(eval(row[0]))
-		new_dataset.append(row_array)
+    dataset = numpy.array(dataset)
+    new_dataset = []
+    for row in dataset:
+        row_array = numpy.array(eval(row[0]))
+        new_dataset.append(row_array)
 
-	return numpy.array(new_dataset)
+    return numpy.array(new_dataset)
 
 
 ## File watchers
 
 def watch_file(filename, interval):
     file = open(filename, "r")
-    file.seek(0,2)
+    file.seek(0, 2)
     while True:
         line = file.readline()
         if not line:
             time.sleep(interval)
             continue
         yield line
+
 
 def tail_F(some_file):
     first_call = True
@@ -124,5 +138,3 @@ def tail_F(some_file):
                         yield line + '\n'
         except IOError:
             yield ''
-
-
--- ocsvm.py
+++ ocsvm.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
-import matplotlib.font_manager
 from sklearn import svm
 import numpy as np
+import json
 import pandas
 import helpers
 from keras.preprocessing import sequence
 from sklearn import metrics
 import lsanomaly
+
+############ Parameters ###############
+use_lsanomaly = False   # least-squares approach
+load_parameters = False
+nu = 0.1 # Upper bound on the fraction of training errors
+kernel = "rbf"  # ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
+gamma = 0.1 # Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’.
+verbose = True
+#######################################
 
 def print_accuracy(title, datasetY, predictions):
     print title
@@ -17,15 +26,35 @@ def print_accuracy(title, datasetY, predictions):
     print("f1: ", metrics.f1_score(datasetY, predictions))
     print("area under curve (auc): ", metrics.roc_auc_score(datasetY, predictions))
 
-
 def replace_in_list(list, oldChar, newChar):
     for n, i in enumerate(list):
         if i == 'anomaly':
             list[n] = -1.
 
+def save_parameters(model):
+    parameters = model.get_params()
+    parameters = json.dumps(parameters)
+    with open("model_ocsvm.json", "w") as json_file:
+        json_file.write(parameters)
+
+    print("Saved parameters to filesystem")
+
+def load_parameters(model, model_filename = 'model_ocsvm.json'):
+    json_file = open(model_filename, 'r')
+    loaded_parameters = json.loads(json_file)
+    json_file.close()
+    print "Loaded parameters from filesystem."
+
+    return model.set_params(loaded_parameters)
+
 
 def train_with_scikit(trainX, testX):
-    clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+    if (load_parameters == True):
+        parameters = load_parameters()
+        clf = svm.OneClassSVM(parameters)
+    else:
+        clf = svm.OneClassSVM(nu=nu, kernel=kernel, gamma=gamma, verbose=verbose)
+
     clf.fit(trainX)
     y_pred_train = clf.predict(trainX)
     y_pred_test = clf.predict(testX)
@@ -42,6 +71,7 @@ def train_with_lsanomaly(trainX, testX):
     y_pred_train = anomalymodel.predict(trainX)
     y_pred_test = anomalymodel.predict(testX)
 
+    # Process results
     replace_in_list(y_pred_train, 'anomaly', -1)
     replace_in_list(y_pred_test, 'anomaly', -1)
     n_error_train = y_pred_train.count(-1)
@@ -57,7 +87,7 @@ max_vector_length = 30
 train_dataset = pandas.read_csv("./data/train_belma.csv", delimiter=';', engine='python')
 test_dataset = pandas.read_csv("./data/test_belma.csv", delimiter=';', engine='python')
 
-#train_dataset = train_dataset[:len(train_dataset)/6]
+train_dataset = train_dataset[:len(train_dataset)/6]
 
 # Convert strings
 train_dataset_array = helpers.collection_values_to_array(train_dataset)
@@ -70,8 +100,10 @@ testX = sequence.pad_sequences(test_dataset_array, maxlen=max_vector_length) #pa
 assert (trainX.shape[1] == testX.shape[1])
 
 # fit the model
-#y_pred_train, y_pred_test, n_error_train, n_error_test = train_with_scikit(trainX, testX)
-y_pred_train, y_pred_test, n_error_train, n_error_test = train_with_lsanomaly(trainX, testX)
+if (use_lsanomaly == True):
+    y_pred_train, y_pred_test, n_error_train, n_error_test = train_with_lsanomaly(trainX, testX)
+else:
+    y_pred_train, y_pred_test, n_error_train, n_error_test = train_with_scikit(trainX, testX)
 
 #testX_plot = []
 #for n, i in enumerate(testX):
